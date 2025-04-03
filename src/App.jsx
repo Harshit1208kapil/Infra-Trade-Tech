dc679539c0d8775f89c582c978c68a0d4cfc12a0
Harshit1208kapil <109057841+Harshit1208kapil@users.noreply.github.com>
2025-04-03 19:17:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Home from './Components/Home'
+import Login from './Components/Login';
 import Signup from "./Components/SignUp";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Signup />} />
+        <Route path="/login" element = {<Login/>}/>
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
